cb3d90db4b102bf9e4b38f8d184701e30814f219
Nathaniel Motley <nmotley92@gmail.com>
2022-11-15 21:12:52 -0600

    Now displays weater for current and future dates needs icons to be added

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ var cityCurrentEl = document.getElementById('current-city');
 var currentTempEl= document.getElementById('temp');
 var currentWindEl= document.getElementById('wind');
 var currentHumidityEl= document.getElementById('humidity');
+var listEl= document.getElementById('list');
 
-
+var cardsEl = document.getElementsByClassName('card');
 
 var apiKey = "a98edf90fc1d17ee19e0411dd892d134";
 var citys = [];
@@ -14,12 +15,14 @@ var cityLon, cityLat;
 searchButton.addEventListener('click', getCityName);
 
 function getCityName(){
+
     var cityName = citSearchEl.value;
     if(!cityName){
          alert('Please Enter a city'); 
          return;
         }
     cityToLonLat(cityName);
+
 }
 
 function cityToLonLat(city){
@@ -35,6 +38,13 @@ function cityToLonLat(city){
                 return;
             }
             
+            citys.push(city);
+            var listItem= document.createElement("button");
+            listItem.classList.add("list-group-item", "list-group-item-action", "recent-buttons");
+            listItem.textContent= city;
+            localStorage.setItem('citys', JSON.stringify(citys));
+            listEl.appendChild(listItem);
+            
             cityLon= data[0].lon;
             cityLat= data[0].lat;
             currentWeather();
@@ -43,18 +53,42 @@ function cityToLonLat(city){
 }
 
 function currentWeather(){
-    fetch('http://api.openweathermap.org/data/2.5/forecast?lat='+cityLat+'&lon='+cityLon+'&appid='+apiKey, {
+    fetch('http://api.openweathermap.org/data/2.5/weather?lat='+cityLat+'&lon='+cityLon+'&appid='+apiKey+'&units=imperial', {
 
     })
     .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
-        cityCurrentEl.textContent= data.city.name;
         
+        cityCurrentEl.textContent= data.name +" "+ new Date(data.dt*1000).toLocaleDateString();
+        currentTempEl.textContent= "Temp: "+ data.main.temp+' °F';
+        currentWindEl.textContent= "Wind: "+ data.wind.speed+' mph';
+        currentHumidityEl.textContent= "Humidity: "+ data.main.humidity+' %';
+        fiveDayForcast();
       });
-    
-    
+}
 
+function fiveDayForcast(){
+    fetch('http://api.openweathermap.org/data/2.5/forecast?lat='+cityLat+'&lon='+cityLon+'&appid='+apiKey+'&units=imperial', {
+        
+    })
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        for(let i=1;i<=5;i++){
+            var futureDates = document.getElementById('future-days-'+i);
+            var futureTemps = document.getElementById('future-temps-'+i);
+            var futureWind = document.getElementById('future-wind-'+i);
+            var futureHumidity = document.getElementById('future-humidity-'+i);
+            var dayHourCount= (i*8)-1;
+            futureDates.textContent =new Date(data.list[dayHourCount].dt*1000).toLocaleDateString();
+            futureTemps.textContent= "Temp: " + data.list[dayHourCount].main.temp + " °F";
+            futureWind.textContent = "Wind: " + data.list[dayHourCount].wind.speed + " mph";
+            futureHumidity.textContent ="Humidity: " + data.list[dayHourCount].main.humidity+ " %";
+
+        
+        }
+      });
 }
